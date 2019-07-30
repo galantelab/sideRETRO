9611954d9b25f5f16e652bbc1392e080f71a9f5f
Thiago L. A. Miller <tmiller@mochsl.org.br>
2019-07-30 04:21:33 -0300

    FIX: check_sider_cluster.c coverage

--- tests/check_sider_cluster.c
+++ tests/check_sider_cluster.c
@@ -26,20 +26,20 @@ create_db (char *db_path)
 }
 
 static void
-populate_db (sqlite3_stmt *alignment_stmt,
-		const char **id, int (*pos)[2], int size)
+populate_db (sqlite3_stmt *alignment_stmt, int offset,
+		const char *chr, const char **id,
+		int (*pos)[2], int size)
 {
 	int i = 0;
-	int j = 0;
 
-	for (; i < size; i++)
+	for (i = 0; i < size; i++)
 		{
-			db_insert_alignment (alignment_stmt, ++j, id[i], 66,
-					"chr1", pos[i][0], 60, "100M", pos[i][1], pos[i][1],
-					"chr1", 1, ABNORMAL_EXONIC, 1);
-			db_insert_alignment (alignment_stmt, ++j, id[i], 66,
-					"chr1", pos[i][0], 60, "100M", pos[i][1], pos[i][1],
-					"chr1", 1, 0, 1);
+			db_insert_alignment (alignment_stmt, ++offset, id[i], 66,
+					chr, pos[i][0], 60, "100M", pos[i][1], pos[i][1],
+					chr, 1, ABNORMAL_EXONIC, 1);
+			db_insert_alignment (alignment_stmt, ++offset, id[i], 66,
+					chr, pos[i][0], 60, "100M", pos[i][1], pos[i][1],
+					chr, 1, 0, 1);
 		}
 }
 
@@ -67,10 +67,18 @@ START_TEST (test_cluster)
 	int i = 0;
 	int j = 0;
 
-	int size = 6;
+	const char *chr1 = "chr1";
+	const char *chr2 = "chr2";
 
-	const char *id[] = {"id1", "id2", "id3",
+	const char *id1[] = {"id1", "id2", "id3",
 		"id4", "id5", "id6"};
+
+	const char *id2[] = {"id7", "id8", "id9",
+		"id10", "id11", "id12"};
+
+	// Number of positions
+	// for each chromosome
+	int size = 6;
 
 	int pos[][2] = {
 		{1000, 101},
@@ -90,14 +98,24 @@ START_TEST (test_cluster)
 		{1, 6, 3, 3},
 		{2, 8, 2, 2},
 		{2, 10, 3, 3},
-		{2, 12, 2, 2}
+		{2, 12, 2, 2},
+		{3, 14, 3, 3},
+		{3, 16, 3, 3},
+		{3, 18, 3, 3},
+		{4, 20, 2, 2},
+		{4, 22, 3, 3},
+		{4, 24, 2, 2}
 	};
 
 	db = create_db (db_file);
 	alignment_stmt = db_prepare_alignment_stmt (db);
 	clustering_stmt = db_prepare_clustering_stmt (db);
 
-	populate_db (alignment_stmt, id, pos, size);
+	// Populate alignment: To avoid same id constraints,
+	// I need to pass an offset for the id: 0 and the
+	// size number of fragments (size * 2, cause it's paired-end)
+	populate_db (alignment_stmt, 0, chr1, id1, pos, size);
+	populate_db (alignment_stmt, size * 2, chr2, id2, pos, size);
 
 	// RUN
 	cluster (clustering_stmt, eps, min_pts);
@@ -111,7 +129,7 @@ START_TEST (test_cluster)
 			ck_assert_int_eq (db_column_int (search_stmt, j),
 					true[i][j]);
 
-	ck_assert_int_eq (i, size);
+	ck_assert_int_eq (i, size * 2);
 
 	db_finalize (alignment_stmt);
 	db_finalize (clustering_stmt);
