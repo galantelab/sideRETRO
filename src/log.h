d7f634267cd55de8c3613783c4a853a598874d3d
Thiago L. A. Miller <tmiller@mochsl.org.br>
2019-10-07 01:36:29 -0300

    ADD: log.h (__FILENAME__): Macro for filename

--- src/log.h
+++ src/log.h
@@ -32,15 +32,17 @@ typedef void (*LogLockFun) (void *udata, int lock);
 
 enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 
-#define log_trace(...) log_log (LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
-#define log_debug(...) log_log (LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
-#define log_info(...)  log_log (LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
-#define log_warn(...)  log_log (LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
-#define log_error(...) log_log (LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+#define __FILENAME__ (strrchr("/" __FILE__, '/') + 1)
+
+#define log_trace(...) log_log (LOG_TRACE, __FILENAME__, __LINE__, __VA_ARGS__)
+#define log_debug(...) log_log (LOG_DEBUG, __FILENAME__, __LINE__, __VA_ARGS__)
+#define log_info(...)  log_log (LOG_INFO,  __FILENAME__, __LINE__, __VA_ARGS__)
+#define log_warn(...)  log_log (LOG_WARN,  __FILENAME__, __LINE__, __VA_ARGS__)
+#define log_error(...) log_log (LOG_ERROR, __FILENAME__, __LINE__, __VA_ARGS__)
 
 #define log_fatal(...) \
 	do { \
-		log_log (LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__); \
+		log_log (LOG_FATAL, __FILENAME__, __LINE__, __VA_ARGS__); \
 		abort (); \
 	} while (0)
 
